6aeb6744c7c422e02adfac25a406c0d9d31a40c0
Aditya Pandey <adityap.star@gmail.com>
2020-02-11 21:41:07 -0600

    proc

--- src/server/index.js
+++ src/server/index.js
@@ -2,14 +2,14 @@ const path = require('path');
 const express = require('express');
 const os = require('os');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
 const foodRoutes = require('./routes/food');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+// app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use('/api/food', foodRoutes);
