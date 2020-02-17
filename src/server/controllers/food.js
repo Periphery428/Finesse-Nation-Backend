23bd663d39e646e09479316e6b7212e5e6e5ce3a
Jeffrey Josol <jjosol100@gmail.com>
2020-02-17 10:05:11 -0600

    Add error output to getPlaces

--- src/server/controllers/food.js
+++ src/server/controllers/food.js
@@ -1,6 +1,7 @@
 let MongoClient = require("mongodb").MongoClient;
 // let mongoUrl = "mongodb://localhost:27017/free_food";
-let mongoUrl = "mongodb+srv://" + process.env.MONGODB_USERNAME + ":" + process.env.MONGODB_PASSWORD + "@mongoclustercs428-pijzh.mongodb.net/free_food?retryWrites=true&w=majority";
+const mongoUrl = "mongodb+srv://" + process.env.MONGODB_USERNAME + ":" + process.env.MONGODB_PASSWORD + "@mongoclustercs428-pijzh.mongodb.net/free_food?retryWrites=true&w=majority";
+const mongoClient = new MongoClient(mongoUrl, {useNewUrlParser:true,  useUnifiedTopology:true });
 
 MongoClient.connect(mongoUrl, function(err, client) {
     if(!err) {
@@ -18,6 +19,9 @@ exports.helloWorld = (req, res, next) => {
 
 exports.getPlaces = (req, res, next) => {
     MongoClient.connect(mongoUrl, mongoOptions, function(err, client) {
+        if (err) {
+            console.log("Error occured in MongoDB: ", err);
+        }
         let db = client.db("free_food");
         let cursor = db.collection('places').find({"city": req.body.city, "state": req.body.state});
         cursor.each(function(err, item) {
