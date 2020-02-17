00111cce3524a77961c0cce32abe271e9d2b550d
Jeffrey Josol <jjosol100@gmail.com>
2020-02-17 10:44:47 -0600

    Revert to original getPlaces()

--- src/server/controllers/food.js
+++ src/server/controllers/food.js
@@ -1,17 +1,17 @@
 let MongoClient = require("mongodb").MongoClient;
 // let mongoUrl = "mongodb://localhost:27017/free_food";
-const mongoUrl = "mongodb+srv://" + process.env.MONGODB_USERNAME + ":" + process.env.MONGODB_PASSWORD + "@mongoclustercs428-pijzh.mongodb.net/free_food?retryWrites=true&w=majority";
-const mongoClient = new MongoClient(mongoUrl, {useNewUrlParser:true,  useUnifiedTopology:true });
+let mongoUrl = "mongodb+srv://" + process.env.MONGODB_USERNAME + ":" + process.env.MONGODB_PASSWORD + "@mongoclustercs428-pijzh.mongodb.net/free_food?retryWrites=true&w=majority";
 
-MongoClient.connect(mongoUrl, function(err, client) {
+const mongoOptions = {
+    connectTimeoutMS: 5000,
+    // useUnifiedTopology:true
+};
+
+MongoClient.connect(mongoUrl, mongoOptions, function(err, db) {
     if(!err) {
         console.log("We are connected to mongodb atlas...");
     }
 });
-
-const mongoOptions = {
-    connectTimeoutMS: 5000
-};
 
 exports.helloWorld = (req, res, next) => {
     res.send("hello there, its working...");
@@ -35,4 +35,25 @@ exports.getPlaces = (req, res, next) => {
         });
         client.close();
     });
+
+    // MongoClient.connect(mongoUrl, mongoOptions, function(err, client) {
+    //     console.log("[test] beginning of MongoClient.connect()");
+    //     if(err) {
+    //        console.log("Error occured in MongoDB: ", err);
+    //     }
+    //     let db = client.db("free_food");
+    //     let cursor = db.collection('places').find({"city": req.body.city, "state": req.body.state});
+    //     console.log("[test] after cursor defined");
+    //     cursor.each(function(err, item) {
+    //         if (item != null) {
+    //             let buildings = [];
+    //             item.buildings.forEach(function(building) {
+    //                 buildings.push(building)
+    //             });
+    //             console.log("[test] buildings = ", buildings);
+    //             res.json({"buildings": buildings});
+    //         }
+    //     });
+    //     client.close();
+    // });
 };
