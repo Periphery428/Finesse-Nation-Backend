f8f853a7847db8d26b7880ed3c588a77166d3fbd
Jeffrey Josol <jjosol100@gmail.com>
2020-03-11 15:13:03 -0500

    Add more test assertions

--- test/user.js
+++ test/user.js
@@ -18,6 +18,7 @@ describe("login", () => {
             .send(newUser)
             .end((err, res) => {
                 expect(res).to.have.status(200);
+                expect(res.body.token).to.be.a("string");
                 expect(res).to.be.json;
                 done();
             });
@@ -34,6 +35,8 @@ describe("login", () => {
             .send(loginCreds)
             .end((err, res) => {
                 expect(res).to.have.status(200);
+                expect(res.body.token).to.be.a("string");
+                expect(res.body.token).to.be.length.greaterThan(0);
                 expect(res).to.be.json;
                 done();
             });
@@ -50,6 +53,24 @@ describe("login", () => {
             .send(loginCreds)
             .end((err, res) => {
                 expect(res).to.have.status(400);
+                expect(res.body.message).to.equal("Incorrect Password !");
+                expect(res).to.be.json;
+                done();
+            });
+    });
+
+    it("it should not login as user with non-existent username", (done) => {
+        let loginCreds = {
+            "emailId": "testmochafake@mochauniversity.edu",
+            "password": "testmochafakepass"
+        };
+        chai.request(server)
+            .post("/api/user/login")
+            .set("api_token", process.env.API_TOKEN)
+            .send(loginCreds)
+            .end((err, res) => {
+                expect(res).to.have.status(400);
+                expect(res.body.message).to.equal("User Not Exist");
                 expect(res).to.be.json;
                 done();
             });
@@ -58,13 +79,14 @@ describe("login", () => {
     it("it should not login as no api_key is passed", (done) => {
         let loginCreds = {
             "emailId": "testmocha1@mochauniversity.edu",
-            "password": "testmocha2pass"
+            "password": "testmocha1pass"
         };
         chai.request(server)
             .post("/api/user/login")
             .send(loginCreds)
             .end((err, res) => {
                 expect(res).to.have.status(401);
+                expect(res.body.message).to.equal("Request is not authorized.");
                 expect(res).to.be.json;
                 done();
             });
@@ -80,6 +102,7 @@ describe("login", () => {
             .send(emailId)
             .end((err, res) => {
                 expect(res).to.have.status(200);
+                expect(res.body.message).to.equal("User (testmocha1@mochauniversity.edu) deleted.");
                 expect(res).to.be.json;
                 done();
             });
