a7da8fe7ea21a963071cb788c0d7bd130dcba44f
Shilpa Rani <shilpa@Shilpas-MacBook-Pro.local>
2020-03-31 20:30:50 -0500

    added comment files

---
+++ src/server/model/comment.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose"),
+
+    CommentSchema = mongoose.Schema({
+        "eventId": {
+            "type": String,
+            // "required": true
+            required: 'Please enter correct event id.',
+            trim: true
+        },
+        "emailId": {
+            "type": String,
+            // "required": true
+            // unique:true,
+            required: 'Please enter the email id',
+            trim: true,
+            // index: true
+            // lowercase:true,
+            // validate: [{ validator: value => isEmail(value), msg: 'Invalid email.' }]
+        },
+        "comment`": {
+            "type": String,
+            "required": true
+        }
+    });
+
+// Export model comment with CommentSchema
+module.exports = mongoose.model("finesse_nation_comments", CommentSchema);
+
+
