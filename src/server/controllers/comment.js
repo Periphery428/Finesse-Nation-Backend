7c353ce47397b83d9ac24fb3496fe5457a1ffdbb
Jeffrey Josol <jjosol100@gmail.com>
2020-04-14 15:15:44 -0500

    Comment out untested code

--- src/server/controllers/comment.js
+++ src/server/controllers/comment.js
@@ -2,71 +2,65 @@ const {body, validationResult} = require("express-validator");
 
 const Comment = require("../model/comment");
 
-exports.getComments = function(req, res) {
-
-    Comment.find({"eventId": req.body.eventId}).exec(function(err, listComments) {
-        if(err) {
-            console.log("Error: unable to get events");
-            res.status(400).end();
-        } else {
-            res.json(listComments);
-        }
-    });
-};
+// exports.getComments = function(req, res) {
+//     Comment.find({"eventId": req.body.eventId}).exec(function(err, listComments) {
+//         if(err) {
+//             console.log("Error: unable to get events");
+//             res.status(400).end();
+//         } else {
+//             res.json(listComments);
+//         }
+//     });
+// };
 
 
 //TODO: FIX ISSUE
 //'comment`': MongooseError [ValidatorError]: Path `comment`` is required.
 
-exports.addComment = [
-    // Validate fields
-    body("eventId", "Please enter a valid event title").isLength({min: 1}).trim(),
-    // body("description", "Please enter a valid description").isLength({min: 1}).trim(),
-    body("emailId", "Please enter a valid location").isLength({min: 1}).trim(),
-    // body("duration", "Please enter a valid duration").isLength({min: 1}).trim(),
-    body("comment", "Please enter a valid time posted").isLength({min: 1}).trim(),
-    // body("image", "Please enter a valid image string binary").isLength({min: 1}).trim(),
-
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            console.log("Error Happened");
-            return res.status(400).json({
-                errors: errors.array()
-            });
-        }
-
-        const {eventId, emailId, comment} = req.body;
-
-        let newComment = new Comment({
-            "eventId": eventId,
-            "emailId": emailId,
-            "comment": comment
-        });
-
-        await newComment.save(function(err) {
-            if(err) {
-                res.send({"Error": "adding new comment = " + comment});
-                console.log(err);
-                res.status(400).end();
-            } else {
-                let logMessage = "Success: added new comment = " + comment;
-                console.log(logMessage);
-                res.send(logMessage);
-            }
-        });
-    }
-];
-
-
+// exports.addComment = [
+//     // Validate fields
+//     body("eventId", "Please enter a valid event title").isLength({min: 1}).trim(),
+//     // body("description", "Please enter a valid description").isLength({min: 1}).trim(),
+//     body("emailId", "Please enter a valid location").isLength({min: 1}).trim(),
+//     // body("duration", "Please enter a valid duration").isLength({min: 1}).trim(),
+//     body("comment", "Please enter a valid time posted").isLength({min: 1}).trim(),
+//     // body("image", "Please enter a valid image string binary").isLength({min: 1}).trim(),
+//
+//     async (req, res) => {
+//         const errors = validationResult(req);
+//         if (!errors.isEmpty()) {
+//             console.log("Error Happened");
+//             return res.status(400).json({
+//                 errors: errors.array()
+//             });
+//         }
+//
+//         const {eventId, emailId, comment} = req.body;
+//
+//         let newComment = new Comment({
+//             "eventId": eventId,
+//             "emailId": emailId,
+//             "comment": comment
+//         });
+//
+//         await newComment.save(function(err) {
+//             if(err) {
+//                 res.send({"Error": "adding new comment = " + comment});
+//                 console.log(err);
+//                 res.status(400).end();
+//             } else {
+//                 let logMessage = "Success: added new comment = " + comment;
+//                 console.log(logMessage);
+//                 res.send(logMessage);
+//             }
+//         });
+//     }
+// ];
 
 // await this.findOneAndUpdate(
 //     conditions, { $set: data }, { new: true, runValidators: true }
 // ).exec()
 // return article
-
-
-
 
 // exports.updateComment = [
 //     // Validate fields
