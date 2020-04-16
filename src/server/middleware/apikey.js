eb91157c671ad58041e003cd9c8a8d6463c069de
Jeffrey Josol <jjosol100@gmail.com>
2020-04-16 12:39:55 -0500

    Include admin path to api key validation

--- src/server/middleware/apikey.js
+++ src/server/middleware/apikey.js
@@ -1,8 +1,7 @@
 const apiToken = process.env.API_TOKEN
 
 module.exports = function(req, res, next) {
-    if(req.originalUrl.startsWith('/admin') || req.originalUrl.startsWith('/static') ||
-        req.originalUrl.startsWith('/favicon.ico')) {
+    if(req.originalUrl.startsWith('/static') || req.originalUrl.startsWith('/favicon.ico')) {
         next();
     } else {
         if(req.headers.api_token !== apiToken) {
