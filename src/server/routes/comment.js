c142f51fc730803d9444aa23a2b92c130a881ed9
Jeffrey Josol <jjosol100@gmail.com>
2020-05-04 16:47:23 -0500

    Swagger jsdoc for event.js

--- src/server/routes/comment.js
+++ src/server/routes/comment.js
@@ -1,9 +1,56 @@
 const express = require('express'),
-    router = express.Router();
+router = express.Router();
 
 const commentController = require('../controllers/comment');
 
+/**
+ * @swagger
+ * /api/comment/{eventId}:
+ *    get:
+ *      tags:
+ *          - Comments
+ *      summary: Get list of comments for an event.
+ *      parameters:
+ *        - name: eventId
+ *          in: path
+ *          description: Id of event.
+ *      responses:
+ *        200:
+ *          description: Successfully return list of comments for an event.
+ *        400:
+ *          description: Fails to return list of comments for an event.
+ */
 router.get('/:eventId', commentController.getComments);
+
+/**
+ * @swagger
+ * /api/comment:
+ *    post:
+ *      tags:
+ *          - Comments
+ *      summary: Add a new comment for an event.
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - name: body
+ *          in: body
+ *          schema:
+ *            type: object
+ *            properties:
+ *              eventId:
+ *                type: string
+ *              emailId:
+ *                type: string
+ *              comment:
+ *                type: string
+ *              postedTime:
+ *                type: string
+ *      responses:
+ *        200:
+ *          description: Successfully added comment for an event.
+ *        400:
+ *          description: Error on comment save or input validation failed.
+ */
 router.post('/', commentController.addComment);
 
 module.exports = router;
--- src/server/routes/event.js
+++ src/server/routes/event.js
@@ -3,9 +3,130 @@ const router = express.Router();
 
 const eventController = require('../controllers/event');
 
+/**
+ * @swagger
+ * /api/food/getEvents:
+ *    get:
+ *      tags:
+ *          - Events
+ *      summary: Get list of all events.
+ *      responses:
+ *        200:
+ *          description: Successfully return list of events.
+ *        400:
+ *          description: Fails to return list of events.
+ */
 router.get('/getEvents', eventController.getEvents);
+
+/**
+ * @swagger
+ * /api/food/addEvent:
+ *    post:
+ *      tags:
+ *          - Events
+ *      summary: Add a new event.
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - name: body
+ *          in: body
+ *          schema:
+ *            type: object
+ *            properties:
+ *              eventTitle:
+ *                type: string
+ *              emailId:
+ *                type: string
+ *              school:
+ *                type: string
+ *              description:
+ *                type: string
+ *              location:
+ *                type: string
+ *              isActive:
+ *                type: string
+ *              image:
+ *                type: string
+ *              postedTime:
+ *                type: string
+ *              duration:
+ *                type: string
+ *              category:
+ *                type: string
+ *      responses:
+ *        200:
+ *          description: Successfully added new event.
+ *        400:
+ *          description: Error on adding new event or input validation failed.
+ */
 router.post('/addEvent', eventController.addEvent);
+
+/**
+ * @swagger
+ * /api/food/updateEvent:
+ *    post:
+ *      tags:
+ *          - Events
+ *      summary: Update an event.
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - name: body
+ *          in: body
+ *          schema:
+ *            type: object
+ *            properties:
+ *              eventTitle:
+ *                type: string
+ *              emailId:
+ *                type: string
+ *              school:
+ *                type: string
+ *              description:
+ *                type: string
+ *              location:
+ *                type: string
+ *              isActive:
+ *                type: string
+ *              image:
+ *                type: string
+ *              postedTime:
+ *                type: string
+ *              duration:
+ *                type: string
+ *              category:
+ *                type: string
+ *      responses:
+ *        200:
+ *          description: Successfully updated event.
+ *        400:
+ *          description: Error on updating event, event non-existing, or input validation failed.
+ */
 router.post('/updateEvent', eventController.updateEvent);
+
+/**
+ * @swagger
+ * /api/food/deleteEvent:
+ *    post:
+ *      tags:
+ *          - Events
+ *      summary: Delete an event.
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - name: body
+ *          in: body
+ *          schema:
+ *            type: object
+ *            properties:
+ *              eventId:
+ *                type: string
+ *      responses:
+ *        200:
+ *          description: Successfully deleted event.
+ *        400:
+ *          description: Error on deleting event.
+ */
 router.post('/deleteEvent', eventController.deleteEvent);
 
 module.exports = router;
